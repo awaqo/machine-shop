c6816ab64e6fe2281d45955de1a2a5ba7e759643
awaqo <aqiljawadalfurqon@gmail.com>
2020-08-13 22:30:07 +0700

    first commit

---
+++ galeri.php
@@ -0,0 +1,87 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bengkel Orang</title>
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.13.0/css/all.min.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+    <div class="container-fluid bg-top-header px-5">
+        <div class="row">
+        <a href="https://api.whatsapp.com/send?phone=08176741866" target="_blank" class="nav-link text-light mr-5"><i class="fas fa-phone"></i> 08176741866</a>
+            <a href="#" class="nav-link text-light"><i class="fas fa-map-marked-alt"></i> Jl. Sutomo RT31/08 Desa Wanarata, Kecamatan Bantarbolang, Kabupaten Pemalang</a>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-header sticky-top">
+        <div class="navbar-brand font-weight-bold text-light px-4">
+            <img src="assets/img/log-car.png" width="45" height="45" class="img-fluid pt-2 mr-1 d-inline-block align-top">
+            Nama Perusahaan
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse px-2" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item mr-2">
+                    <a class="nav-link text-light text-capitalize font-weight-bold" href="home">Home</a>
+                </li>
+                <li class="nav-item active mr-2">
+                    <a class="menu nav-link text-light text-capitalize font-weight-bold" href="galeri">Galeri</a>
+                </li>
+                <li class="nav-item mr-2">
+                    <a class="menu nav-link text-light text-capitalize font-weight-bold" href="kontak">Kontak Kami</a>
+                </li>
+                <li class="nav-item mr-2">
+                    <a class="menu nav-link text-light text-capitalize font-weight-bold" href="profil">Profil</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    <div class="container-fluid">
+        <h1 class="border">Galeri</h1>
+    </div>
+    
+    <a href="https://api.whatsapp.com/send?phone=08176741866" target="_blank" class="wa-pop btn bg-header text-light rounded-circle" style="position: fixed; bottom: 80px; right: 30px; height: 45px; display: none"><i class="fab fa-fw fa-whatsapp pt-2"></i></a>
+    <!-- scroll to top -->
+    <a href="#" class="scrollToTop btn bg-header text-light" style="position: fixed; bottom: 30px; right: 30px; display: none"><i class="fas fa-fw fa-chevron-up"></i></a>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script type="text/javascript">
+    // AOS init
+        AOS.init();
+    // scroll to top
+        $(document).ready(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop() > 200){
+                    $('.scrollToTop').fadeIn();
+                } else {
+                    $('.scrollToTop').fadeOut();
+                }
+            });
+            $('.scrollToTop').click(function(){
+                $('html,body').animate({scrollTop: 0}, 1000)
+            })
+        });
+
+        // pop up wa
+        $(document).ready(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop() > 200){
+                    $('.wa-pop').fadeIn();
+                } else {
+                    $('.wa-pop').fadeOut();
+                }
+            });
+        });
+    </script>
+</body>
+</html>
---
+++ home.php
@@ -0,0 +1,150 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Bengkel Orang</title>
+    <link rel="stylesheet" href="assets/css/style.css">
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/css/bootstrap.min.css" integrity="sha384-JcKb8q3iqJ61gNV9KGb8thSsNjpSL0n8PARn9HuZOnIxN0hoP+VmmDGMN5t9UJ0Z" crossorigin="anonymous">
+    <link href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.14.0/css/all.min.css" rel="stylesheet" type="text/css">
+    <!-- <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.14.0/css/all.css" integrity="sha384-HzLeBuhoNPvSl5KYnjx0BT+WB0QEEqLprO+NBkkk5gbc67FTaL7XIGa2w1L0Xbgc" crossorigin="anonymous"> -->
+    <link href="https://unpkg.com/aos@2.3.1/dist/aos.css" rel="stylesheet">
+</head>
+<body>
+    <div class="container-fluid bg-top-header px-5">
+        <div class="row">
+            <a href="https://api.whatsapp.com/send?phone=08176741866" target="_blank" class="nav-link text-light mr-5"><i class="fas fa-phone"></i> 08176741866</a>
+            <a href="#" class="nav-link text-light"><i class="fas fa-map-marked-alt"></i> Jl. Sutomo RT31/08 Desa Wanarata, Kecamatan Bantarbolang, Kabupaten Pemalang</a>
+        </div>
+    </div>
+    <nav class="navbar navbar-expand-lg navbar-dark bg-header sticky-top">
+        <div class="navbar-brand font-weight-bold text-light px-4">
+            <img src="assets/img/log-car.png" width="45" height="45" class="img-fluid pt-2 mr-1 d-inline-block align-top">
+            Nama Perusahaan
+        </div>
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+
+        <div class="collapse navbar-collapse px-2" id="navbarSupportedContent">
+            <ul class="navbar-nav ml-auto">
+                <li class="nav-item active mr-2">
+                    <a class="nav-link text-light text-capitalize font-weight-bold" href="home">Home</a>
+                </li>
+                <li class="nav-item mr-2">
+                    <a class="menu nav-link text-light text-capitalize font-weight-bold" href="galeri">Galeri</a>
+                </li>
+                <li class="nav-item mr-2 dropdown">
+                    <a class="menu nav-link text-light text-capitalize font-weight-bold" href="kontak">Kontak Kami</a>
+                </li>
+                <li class="nav-item mr-2">
+                    <a class="menu nav-link text-light text-capitalize font-weight-bold" href="profil">Profil</a>
+                </li>
+            </ul>
+        </div>
+    </nav>
+    
+    <div class="bg-img d-flex justify-content-center align-items-center">
+        <div class="slogan mx-3">
+            <h1 class="text-light text-center">Kami Memberikan Layanan Terbaik</h1>
+        </div>
+    </div>
+    <div class="container-fluid mt-5 mb-5">
+        <h2 class="text-center font-weight-bold text-dark">Kenapa memilih kami?</h2>
+        <div class="row d-flex justify-content-center mt-3">
+            <div class="card rounded-0 col-md-3 mx-3 my-2" data-aos="fade-right" data-aos-duration="2000">
+                <div class="card-body">
+                    <h3 class="card-title text-center">Pelayanan Cepat</h3>
+                    <hr class="mx-auto" style="width: 50%; border-top: 2px solid #2f3697">
+                    <p class="card-text">Kami percaya dengan memberikan layanan yang cepat dapat membuat pelanggan puas. Dengan "Pelayanan Satu Hari" kami dapat menyelesaikan pekerjaan pasti dalam hari yang sama.</p>
+                </div>
+            </div>
+            <div class="card rounded-0 col-md-3 mx-3 my-2" data-aos="fade-up" data-aos-duration="2000">
+                <div class="card-body">
+                    <h3 class="card-title text-center">Kualitas Terbaik</h3>
+                    <hr class="mx-auto" style="width: 50%; border-top: 2px solid #2f3697">
+                    <p class="card-text">Seluruh peralatan dan bahan yang kami gunakan terjamin kualitasnya, karena kualitas produk terbaik tentunya akan membuat pelanggan sangat puas.</p>
+                </div>
+            </div>
+            <div class="card rounded-0 col-md-3 mx-3 my-2" data-aos="fade-left" data-aos-duration="2000">
+                <div class="card-body">
+                    <h3 class="card-title text-center">Garansi 5 Tahun</h3>
+                    <hr class="mx-auto" style="width: 50%; border-top: 2px solid #2f3697">
+                    <p class="card-text">Dengan garansi yang cukup lama pastinya akan membuat pelanggan merasa lebih tenang karena kami menjamin akan garansi tersebut.</p>
+                </div>
+            </div>
+        </div>
+        <h1 class="border mt-4 mb-5" style="height: 400px">Home</h1>
+
+    </div>
+    <nav class="fixed-botom navbar-light bg-dark">
+        <div class="container-fluid py-3">
+            <div class="row d-flex justify-content-left mt-3">
+                <div class="card border-0 rounded-0 col-md-4 bg-dark text-light">
+                    <div class="card-body">
+                        <h6 class="card-title text-left">Tentang Kami</h6>
+                        <hr style="width: 50%; border-top: 2px solid #5059d8">
+                        <p class="card-text">.</p>
+                    </div>
+                </div>
+                <div class="card border-0 rounded-0 col-md-4 bg-dark text-light">
+                    <div class="card-body">
+                        <h6 class="card-title text-left">Postingan Terbaru</h6>
+                        <hr style="width: 50%; border-top: 2px solid #5059d8">
+                        <div class="col">
+                            <a href="#" class="text-light">-</a>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div class="text-light py-3 text-center w-100" style="font-size: 14px; background-color: #262c30">
+            <div class="mb-2">
+                <a href="#" class="btn text-light"><i class="fab fa-fw fa-youtube"></i></a> 
+                <a href="#" class="btn text-light px-0"><i class="fab fa-fw fa-instagram"></i></a> 
+                <a href="#" class="btn text-light"><i class="fab fa-fw fa-facebook"></i></a>
+            </div>
+            <i class="far fa-fw fa-copyright"></i> Copyright <script>document.write(new Date().getFullYear())</script> Perusahaan
+        </div>
+    </nav>
+    <!-- scroll to top -->
+    <a href="https://api.whatsapp.com/send?phone=08176741866" target="_blank" class="wa-pop btn bg-header text-light rounded-circle" style="position: fixed; bottom: 80px; right: 30px; height: 45px; display: none"><i class="fab fa-fw fa-whatsapp pt-2"></i></a>
+    <a href="#" class="scrollToTop btn bg-header text-light" style="position: fixed; bottom: 30px; right: 30px; display: none"><i class="fas fa-fw fa-chevron-up"></i></a>
+
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+    <!-- <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script> -->
+    <script src="https://cdn.jsdelivr.net/npm/popper.js@1.16.1/dist/umd/popper.min.js" integrity="sha384-9/reFTGAW83EW2RDu2S0VKaIzap3H66lZH81PoYlFhbGU+6BZp6G7niu735Sk7lN" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js" integrity="sha384-B4gt1jrGC7Jh4AgTPSdUtOBvfO8shuf57BaghqFfPlYxofvL8/KUEfYiJOMMV+rV" crossorigin="anonymous"></script>
+    <script src="https://unpkg.com/aos@2.3.1/dist/aos.js"></script>
+
+    <script type="text/javascript">
+    // AOS init
+        AOS.init();
+    // scroll to top
+        $(document).ready(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop() > 200){
+                    $('.scrollToTop').fadeIn();
+                } else {
+                    $('.scrollToTop').fadeOut();
+                }
+            });
+            $('.scrollToTop').click(function(){
+                $('html,body').animate({scrollTop: 0}, 1000)
+            })
+        });
+
+        // pop up wa
+        $(document).ready(function(){
+            $(window).scroll(function(){
+                if($(this).scrollTop() > 200){
+                    $('.wa-pop').fadeIn();
+                } else {
+                    $('.wa-pop').fadeOut();
+                }
+            });
+        });
+    </script>
+
+</body>
+</html>
